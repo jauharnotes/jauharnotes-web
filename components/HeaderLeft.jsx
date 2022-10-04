07b3a6e4380bf3f94165db8ebe9350a85f8da2fb
Jauharuddin <jauharnotes@gmail.com>
2022-10-04 09:27:55 +0700

    update resume

--- components/HeaderLeft.jsx
+++ components/HeaderLeft.jsx
@@ -3,18 +3,36 @@ import React from 'react'
 
 function HeaderLeft() {
   return (
-    <nav className='hidden md:flex absolute bottom-0 left-0 mb-44 ml-[70px]'>
-        <ul className='flex flex-col gap-14 items-center '>
-            <li className='vertical-lr'>
-                <a href='https://github.com/jauharnotes' target='_blank' className='font-bold hover:text-base hover:underline hover:underline-offset-8'>my open source projects</a>
-            </li>
-            <li className='vertical-lr'>
-                <a href='https://www.fiverr.com/joharid' target='_blank' className='font-bold hover:text-base hover:underline hover:underline-offset-8'>fiverr</a>
-            </li>
-            <li className='vertical-lr'>
-                <a href='https://drive.google.com/file/d/1sTsUVD8sxudw86jnH6KoXlDo9139m_Bo/view' target='_blank' className='font-bold hover:text-base hover:underline hover:underline-offset-8'>download cv</a>
-            </li>
-        </ul>
+    <nav className="absolute bottom-0 left-0 mb-44 ml-[70px] hidden md:flex">
+      <ul className="flex flex-col items-center gap-14 ">
+        <li className="vertical-lr">
+          <a
+            href="https://github.com/jauharnotes"
+            target="_blank"
+            className="font-bold hover:text-base hover:underline hover:underline-offset-8"
+          >
+            my open source projects
+          </a>
+        </li>
+        <li className="vertical-lr">
+          <a
+            href="https://www.fiverr.com/joharid"
+            target="_blank"
+            className="font-bold hover:text-base hover:underline hover:underline-offset-8"
+          >
+            fiverr
+          </a>
+        </li>
+        <li className="vertical-lr">
+          <a
+            href="https://drive.google.com/file/d/1tA1b7Wl2oPMzZ2mj4FXOuMQ1TnXdoQa1/view"
+            target="_blank"
+            className="font-bold hover:text-base hover:underline hover:underline-offset-8"
+          >
+            download cv
+          </a>
+        </li>
+      </ul>
     </nav>
   )
 }
